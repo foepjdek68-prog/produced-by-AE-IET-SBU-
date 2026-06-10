bfb13f5a8e4c9fa1bfbd9d07cb6f33a4a027b820
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 01:20:50 +0700

    Update 2_Map.py

--- pages/2_Map.py
+++ pages/2_Map.py
@@ -1,1 +1,35 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
 
+st.title("🗺️ Thailand Monitoring Map")
+
+map_df = pd.DataFrame({
+
+    "lat":[13.7563],
+    "lon":[100.5018],
+    "value":[430]
+
+})
+
+st.pydeck_chart(
+    pdk.Deck(
+
+        initial_view_state=pdk.ViewState(
+            latitude=13.7563,
+            longitude=100.5018,
+            zoom=5
+        ),
+
+        layers=[
+
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=map_df,
+                get_position='[lon, lat]',
+                get_radius=50000
+            )
+
+        ]
+    )
+)
