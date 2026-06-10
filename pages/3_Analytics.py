36266d997016b10e5c2f322ad4483847cc65fbad
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 02:28:03 +0700

    Update 3_Analytics.py

--- pages/3_Analytics.py
+++ pages/3_Analytics.py
@@ -1,37 +1,10 @@
 import streamlit as st
-
 from utils.database import load_data
-
-st.title("📈 Analytics")
 
 df = load_data()
 
-if df.empty:
-    st.warning("ไม่มีข้อมูล")
-    st.stop()
+st.title("📊 Analytics")
 
-c1, c2, c3 = st.columns(3)
-
-c1.metric(
-    "Average CO2",
-    round(df["CO2"].mean(), 2)
-)
-
-c2.metric(
-    "Maximum CO2",
-    round(df["CO2"].max(), 2)
-)
-
-c3.metric(
-    "Minimum CO2",
-    round(df["CO2"].min(), 2)
-)
-
-st.divider()
-
-st.subheader("Correlation Matrix")
-
-st.dataframe(
-    df.corr(numeric_only=True),
-    use_container_width=True
-)
+st.metric("Avg CO2", df["CO2"].mean())
+st.metric("Max CO2", df["CO2"].max())
+st.metric("Min CO2", df["CO2"].min())
