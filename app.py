291f838a93bf356328483d84eb8a2d5b11334503
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 02:57:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ st.set_page_config(
     layout="wide"
 )
 
-# -------------------
-# Load Data
-# -------------------
+# โหลดข้อมูล
 
 df = load_data()
 
@@ -22,9 +20,9 @@ if df.empty:
 
 latest = df.iloc[-1]
 
-# -------------------
+# ------------------------
 # Header
-# -------------------
+# ------------------------
 
 st.title("🌍 Dashboard Tracking Greenhouse Gases Emission")
 
@@ -34,9 +32,9 @@ st.caption(
 
 st.markdown("---")
 
-# -------------------
+# ------------------------
 # KPI
-# -------------------
+# ------------------------
 
 c1,c2,c3,c4,c5,c6 = st.columns(6)
 
@@ -44,20 +42,20 @@ c1.metric("CO₂", round(latest["CO2"],1))
 c2.metric("CH₄", round(latest["CH4"],1))
 c3.metric("NO₂", round(latest["NO2"],1))
 c4.metric("PM2.5", round(latest["PM25"],1))
-c5.metric("Temp", round(latest["Temp"],1))
-c6.metric("Humidity", round(latest["Humidity"],1))
+c5.metric("อุณหภูมิ", round(latest["Temp"],1))
+c6.metric("ความชื้น", round(latest["Humidity"],1))
 
 st.markdown("---")
 
-# -------------------
-# Graph + Summary
-# -------------------
+# ------------------------
+# กราฟ + สรุป
+# ------------------------
 
 left,right = st.columns([4,1])
 
 with left:
 
-    st.subheader("📈 Emission Trend")
+    st.subheader("📈 แนวโน้มข้อมูล")
 
     selected = st.selectbox(
         "เลือกข้อมูล",
@@ -88,24 +86,24 @@ with left:
 
 with right:
 
-    st.subheader("📊 Overview")
+    st.subheader("📊 สรุปข้อมูล")
 
     st.metric(
-        "Last Update",
+        "ข้อมูลล่าสุด",
         str(df.iloc[-1]["Date"])[:16]
     )
 
     st.metric(
-        "Records",
+        "จำนวนข้อมูล",
         len(df)
     )
 
     st.metric(
-        "Average",
+        "ค่าเฉลี่ย",
         round(df[selected].mean(),2)
     )
 
     st.metric(
-        "Maximum",
+        "ค่าสูงสุด",
         round(df[selected].max(),2)
     )
