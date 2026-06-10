f0068d239c5e2db56dc068ad6c78de8c338b6a67
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 02:27:31 +0700

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,48 +1,54 @@
 import streamlit as st
 import plotly.express as px
-
 from utils.database import load_data
-
-st.title("📊 Dashboard")
 
 df = load_data()
 
-if df.empty:
-    st.warning("ไม่มีข้อมูล")
-    st.stop()
-
 latest = df.iloc[-1]
 
-c1, c2, c3, c4 = st.columns(4)
+c1,c2,c3,c4,c5,c6 = st.columns(6)
 
-c1.metric("CO₂", round(latest["CO2"], 1))
-c2.metric("CH₄", round(latest["CH4"], 1))
-c3.metric("NO₂", round(latest["NO2"], 1))
-c4.metric("PM2.5", round(latest["PM25"], 1))
+c1.metric("CO2", round(latest["CO2"],1))
+c2.metric("CH4", round(latest["CH4"],1))
+c3.metric("NO2", round(latest["NO2"],1))
+c4.metric("PM2.5", round(latest["PM25"],1))
+c5.metric("Temp", round(latest["Temp"],1))
+c6.metric("Humidity", round(latest["Humidity"],1))
 
-st.divider()
+selected = st.selectbox("เลือกตัวแปร", ["CO2","CH4","NO2","PM25"])
 
-selected = st.selectbox(
-    "เลือกข้อมูล",
-    ["CO2", "CH4", "NO2", "PM25"]
-)
-
-fig = px.line(
-    df.tail(168),
-    x="Date",
-    y=selected,
-    title=f"{selected} Trend (7 วันล่าสุด)",
-    template="plotly_dark"
-)
-
+fig = px.line(df.tail(168), x="Date", y=selected)
 st.plotly_chart(fig, use_container_width=True)
 
-st.divider()
+def emission_score(df):
+    latest = df.iloc[-1]
+    return round(
+        latest["CO2"]*0.5 +
+        latest["CH4"]*0.3 +
+        latest["NO2"]*0.2,
+        2
+    )
 
-st.subheader("⚙️ Data Pipeline")
+st.metric("Emission Score", emission_score(df))
 
-a, b, c = st.columns(3)
+st.subheader("Data Sources")
 
-a.success("API Connected")
-b.success("Database Updated")
-c.success("Analytics Complete")
+st.success("Air4Thai")
+st.success("TMD")
+st.warning("OpenAQ (Planned)")
+st.warning("Sentinel-5P (Planned)")
+
+###########
+st.markdown("### Project Objective")
+
+st.info("""
+รวมข้อมูลมลพิษจากหลายแหล่งมาไว้ใน Dashboard เดียว
+เพื่อช่วยการติดตามและวิเคราะห์สิ่งแวดล้อม
+""")
+
+st.caption("""
+Developed by:
+- Member 1
+- Member 2
+- Member 3
+""")
