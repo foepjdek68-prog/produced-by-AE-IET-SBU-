a71e7113eba40c040244a430df28aadf4f937a89
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 02:00:08 +0700

    Update 2_Map.py

--- pages/2_Map.py
+++ pages/2_Map.py
@@ -5,31 +5,25 @@ import pydeck as pdk
 st.title("🗺️ Thailand Monitoring Map")
 
 map_df = pd.DataFrame({
-
-    "lat":[13.7563],
-    "lon":[100.5018],
-    "value":[430]
-
+    "lat": [13.75, 18.79, 7.88],
+    "lon": [100.50, 98.98, 98.39],
+    "city": ["Bangkok", "Chiang Mai", "Phuket"]
 })
 
 st.pydeck_chart(
     pdk.Deck(
-
         initial_view_state=pdk.ViewState(
-            latitude=13.7563,
-            longitude=100.5018,
+            latitude=13.75,
+            longitude=100.50,
             zoom=5
         ),
-
         layers=[
-
             pdk.Layer(
                 "ScatterplotLayer",
                 data=map_df,
                 get_position='[lon, lat]',
                 get_radius=50000
             )
-
         ]
     )
 )
