349f7a0b5f3180f3c38b1e90018d9645224acdcb
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 02:23:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,10 @@
 import streamlit as st
-from utils.database import load_data, save_data
-from utils.api_loader import fetch_data
 
-st.set_page_config(
-    page_title="GHG Tracking System",
-    layout="wide"
-)
+st.set_page_config(layout="wide")
 
-df = load_data()
-
-if df.empty:
-    df = fetch_data()
-    save_data(df)
-
-st.title("🌍 Smart GHG Tracking System")
+st.title("🌍 Integrated Environmental Monitoring Dashboard")
 
 st.markdown("""
-### Tracking Greenhouse Gas Emissions
-
-1️⃣ Data Sources
-
-2️⃣ API Collection
-
-3️⃣ SQLite Storage
-
-4️⃣ Analytics
-
-5️⃣ Dashboard
-
-6️⃣ Users
+ระบบรวบรวมข้อมูลมลพิษจากหลายแหล่ง
+และแสดงผลใน Dashboard เดียว
 """)
-
-st.success("เลือกเมนูจาก Sidebar")
