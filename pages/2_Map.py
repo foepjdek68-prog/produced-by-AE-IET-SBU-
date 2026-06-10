0e3a5b3ba42da6bf4499ea5bd7f1e889bba72383
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 02:27:42 +0700

    Update 2_Map.py

--- pages/2_Map.py
+++ pages/2_Map.py
@@ -1,29 +1,12 @@
 import streamlit as st
 import pandas as pd
-import pydeck as pdk
 
-st.title("🗺️ Thailand Monitoring Map")
+st.title("🗺️ Map")
 
 map_df = pd.DataFrame({
-    "lat": [13.75, 18.79, 7.88],
-    "lon": [100.50, 98.98, 98.39],
-    "city": ["Bangkok", "Chiang Mai", "Phuket"]
+    "lat":[13.7, 18.8, 7.9],
+    "lon":[100.5, 98.9, 98.4],
+    "city":["Bangkok","Chiang Mai","Phuket"]
 })
 
-st.pydeck_chart(
-    pdk.Deck(
-        initial_view_state=pdk.ViewState(
-            latitude=13.75,
-            longitude=100.50,
-            zoom=5
-        ),
-        layers=[
-            pdk.Layer(
-                "ScatterplotLayer",
-                data=map_df,
-                get_position='[lon, lat]',
-                get_radius=50000
-            )
-        ]
-    )
-)
+st.map(map_df)
