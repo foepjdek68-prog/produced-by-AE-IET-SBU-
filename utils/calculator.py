6dc5b7fe18fa4f0da177d84c21643f33dad9f1c9
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 01:19:45 +0700

    Update calculator.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,1 +1,11 @@
+def emission_score(df):
 
+    latest = df.iloc[-1]
+
+    score = (
+        latest["CO2"] * 0.5 +
+        latest["CH4"] * 0.3 +
+        latest["NO2"] * 0.2
+    )
+
+    return round(score,2)
