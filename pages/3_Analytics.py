8f70d7e429d34eaf20fb92af84519adc1f51619f
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 01:21:08 +0700

    Update 3_Analytics.py

--- pages/3_Analytics.py
+++ pages/3_Analytics.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+from utils.database import load_data
+
+df = load_data()
+
+st.title("📈 Analytics")
+
+if not df.empty:
+
+    st.subheader("Correlation")
+
+    st.dataframe(
+        df.corr(
+            numeric_only=True
+        )
+    )
