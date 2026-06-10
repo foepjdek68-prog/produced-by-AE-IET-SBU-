35b008321ac08513a96f0da5eec6848c4484880c
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 01:19:00 +0700

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,38 @@
+import sqlite3
+import pandas as pd
 
+DB_PATH = "data/ghg_data.db"
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
+def load_data():
+
+    conn = get_connection()
+
+    try:
+        df = pd.read_sql(
+            "SELECT * FROM ghg_data",
+            conn
+        )
+
+    except:
+        df = pd.DataFrame()
+
+    conn.close()
+
+    return df
+
+
+def save_data(df):
+
+    conn = get_connection()
+
+    df.to_sql(
+        "ghg_data",
+        conn,
+        if_exists="replace",
+        index=False
+    )
+
+    conn.close()
