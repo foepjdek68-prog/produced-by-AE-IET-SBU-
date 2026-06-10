6966ba718e4cbb2a765313892f53ff83838af3b4
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-11 01:50:21 +0700

    Update 4_DataCenter.py

--- pages/4_DataCenter.py
+++ pages/4_DataCenter.py
@@ -1,11 +1,24 @@
 import streamlit as st
-from utils.database import load_data
+
+from utils.database import (
+    load_data,
+    save_data
+)
+
+from utils.api_loader import fetch_data
 
 df = load_data()
 
+if df.empty:
+
+    st.warning("Database ว่าง กำลังสร้างข้อมูล...")
+
+    df = fetch_data()
+
+    save_data(df)
+
+    st.success(f"สร้างข้อมูลแล้ว {len(df)} rows")
+
 st.write("Rows:", len(df))
 
-if not df.empty:
-    st.write(df.head())
-else:
-    st.warning("Database ว่าง")
+st.dataframe(df.head())
