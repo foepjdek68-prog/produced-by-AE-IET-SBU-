784ac71f596164c4ad51dc209e6ff530a1d90895
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-15 00:49:49 +0700

    Update 2_Download.py

--- pages/2_Download.py
+++ pages/2_Download.py
@@ -1,1 +1,40 @@
+import streamlit as st
 
+from Services.database import load_data
+
+
+
+st.title("📥 Download")
+
+
+
+df = load_data()
+
+
+
+if df.empty:
+
+    st.warning(
+        "ไม่พบข้อมูล"
+    )
+
+
+else:
+
+    st.dataframe(
+        df,
+        use_container_width=True
+    )
+
+
+    csv = df.to_csv(
+        index=False
+    )
+
+
+    st.download_button(
+        "📥 ดาวน์โหลดข้อมูล",
+        csv,
+        "ghg_data.csv",
+        "text/csv"
+    )
