957fd5a85650c76f8fdb045eb123f2b71dcd32f3
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-16 02:19:26 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -60,12 +60,12 @@ st.caption(f"ข้อมูลล่าสุด : {thai_date}")
 
 c1, c2, c3, c4, c5, c6 = st.columns(6)
 
-c1.metric("CO₂ (Carbon Dioxide)", round(float(latest["CO2"]), 1))
-c2.metric("CH₄ (Methane)", round(float(latest["CH4"]), 1))
-c3.metric("NO₂ (Nitrogen Dioxide)", round(float(latest["NO2"]), 1))
-c4.metric("PM 2.5 (Particulate Matter)", round(float(latest["PM25"]), 1))
-c5.metric("Temp (Temperature)", round(float(latest["Temp"]), 1))
-c6.metric("Humidity (Relative Humidity)", round(float(latest["Humidity"]), 1))
+c1.metric("CO₂", round(float(latest["CO2"]), 1))
+c2.metric("CH₄", round(float(latest["CH4"]), 1))
+c3.metric("NO₂", round(float(latest["NO2"]), 1))
+c4.metric("PM 2.5", round(float(latest["PM25"]), 1))
+c5.metric("Temp", round(float(latest["Temp"]), 1))
+c6.metric("Humidity", round(float(latest["Humidity"]), 1))
 
 st.markdown("---")
 
@@ -98,12 +98,12 @@ with left:
     )
 
     options = {
-        "CO₂ (Carbon Dioxide)": "CO2",
-        "CH₄ (Methane)": "CH4",
-        "NO₂ (Nitrogen Dioxide)": "NO2",
-        "PM 2.5 (Particulate Matter)": "PM25",
-        "Temp (Temperature)": "Temp",
-        "Humidity (Relative Humidity)": "Humidity"
+        "CO₂": "CO2",
+        "CH₄": "CH4",
+        "NO₂": "NO2",
+        "PM 2.5": "PM25",
+        "Temp": "Temp",
+        "Humidity": "Humidity"
     }
 
     display_names = {v: k for k, v in options.items()}
@@ -132,7 +132,7 @@ with left:
         selected_labels = st.multiselect(
             "เลือกข้อมูล",
             list(options.keys()),
-            default=["CO₂ (Carbon Dioxide)"]
+            default=["CO₂"]
         )
 
         selected = [options[x] for x in selected_labels]
@@ -191,6 +191,7 @@ with left:
 
 
 with right:
+
     st.subheader("📊 สถานะระบบ")
 
     avg_co2 = df["CO2"].mean()
