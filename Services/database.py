1de8ecb4cc249328dcbed0e965d8acf511144f56
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-22 23:24:16 +0700

    Update database.py

--- Services/database.py
+++ Services/database.py
@@ -1,18 +1,10 @@
 import sqlite3
 import pandas as pd
-import os
-
 
 DB_PATH = "Storage/GHG_Database.db"
 
-
 def get_connection():
-
-    os.makedirs("Storage", exist_ok=True)
-
     return sqlite3.connect(DB_PATH)
-
-
 
 def load_data():
 
@@ -25,40 +17,23 @@ def load_data():
             conn
         )
 
-    except Exception:
+    except:
 
         df = pd.DataFrame()
-
 
     conn.close()
 
     return df
 
-
-
 def save_data(df):
-
-    if df is None or df.empty:
-        return
-
 
     conn = get_connection()
 
-    try:
+    df.to_sql(
+        "ghg_data",
+        conn,
+        if_exists="replace",
+        index=False
+    )
 
-        df.to_sql(
-            "ghg_data",
-            conn,
-            if_exists="replace",
-            index=False
-        )
-
-        conn.commit()
-
-    except Exception as e:
-
-        print("DATABASE ERROR:", e)
-
-    finally:
-
-        conn.close()
+    conn.close()
