b11e2f1df461cd853e28f3eb2c51f5e2253b6d6d
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-22 23:17:19 +0700

    Update api_loader.py

--- Services/api_loader.py
+++ Services/api_loader.py
@@ -2,80 +2,56 @@ import pandas as pd
 import numpy as np
 
 from datetime import datetime
-
 import pytz
 
 
 
 def fetch_data():
 
-
     tz = pytz.timezone(
         "Asia/Bangkok"
     )
 
-
     now = datetime.now(tz)
-
 
 
     dates = pd.date_range(
 
-        start=now - pd.Timedelta(days=365),
+        start=now-pd.Timedelta(days=365),
 
         end=now,
 
-        periods=365*24
+        periods=8760
 
     )
 
 
-
     df = pd.DataFrame({
-
 
         "Date": dates,
 
-
         "CO2": np.random.normal(
-            430,
-            8,
-            len(dates)
+            430,8,len(dates)
         ),
-
 
         "CH4": np.random.normal(
-            1860,
-            15,
-            len(dates)
+            1860,15,len(dates)
         ),
-
 
         "NO2": np.random.normal(
-            42,
-            3,
-            len(dates)
+            42,3,len(dates)
         ),
-
 
         "PM25": np.random.normal(
-            25,
-            5,
-            len(dates)
+            25,5,len(dates)
         ),
-
 
         "Temp": np.random.normal(
-            33,
-            2,
-            len(dates)
+            33,2,len(dates)
         ),
 
-
         "Humidity": np.random.normal(
-            60,
-            4,
-            len(dates)
+            60,4,len(dates)
         )
 
     })
