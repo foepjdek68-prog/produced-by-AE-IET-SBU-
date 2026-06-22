7104953983bc664d7edd1dd0b2f9e31c4de3fa6c
foepjdek68-prog <foepjdek68@gmail.com>
2026-06-22 23:12:01 +0700

    Update api_loader.py

--- Services/api_loader.py
+++ Services/api_loader.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 import numpy as np
 
 from datetime import datetime
@@ -8,13 +7,13 @@ import pytz
 
 
 
-
-
 def fetch_data():
 
 
+    tz = pytz.timezone(
+        "Asia/Bangkok"
+    )
 
-    tz = pytz.timezone("Asia/Bangkok")
 
     now = datetime.now(tz)
 
@@ -26,7 +25,7 @@ def fetch_data():
 
         end=now,
 
-        periods=365 * 24
+        periods=365*24
 
     )
 
@@ -35,27 +34,51 @@ def fetch_data():
     df = pd.DataFrame({
 
 
-
         "Date": dates,
 
 
-
-        "CO2": np.random.normal(430, 8, len(dates)),
-
-        "CH4": np.random.normal(1860, 15, len(dates)),
-
-        "NO2": np.random.normal(42, 3, len(dates)),
-
-        "PM25": np.random.normal(25, 5, len(dates)),
-
-        "Temp": np.random.normal(33, 2, len(dates)),
-
-        "Humidity": np.random.normal(60, 4, len(dates))
+        "CO2": np.random.normal(
+            430,
+            8,
+            len(dates)
+        ),
 
 
+        "CH4": np.random.normal(
+            1860,
+            15,
+            len(dates)
+        ),
+
+
+        "NO2": np.random.normal(
+            42,
+            3,
+            len(dates)
+        ),
+
+
+        "PM25": np.random.normal(
+            25,
+            5,
+            len(dates)
+        ),
+
+
+        "Temp": np.random.normal(
+            33,
+            2,
+            len(dates)
+        ),
+
+
+        "Humidity": np.random.normal(
+            60,
+            4,
+            len(dates)
+        )
 
     })
-
 
 
     return df.round(2)
